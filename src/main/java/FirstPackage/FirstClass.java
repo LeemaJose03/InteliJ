301fd258cc3b0cd066bb7f1e24edd5963adc35b7
LeemaJose03 <leejo1803@gmail.com>
2024-01-11 19:07:18 +0530

    First commit

---
+++ src/main/java/FirstPackage/FirstClass.java
@@ -0,0 +1,18 @@
+package FirstPackage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+
+    public static void main(String[] args){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =  new ChromeDriver();
+        driver.get("https://www.guvi.in");
+        driver.manage().window().maximize();
+        driver.close();
+
+    }
+}
